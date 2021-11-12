8f64b551045183104f94edffafba1e68d4cf944c
Dallin <dallinhatton@gmail.com>
2021-11-12 13:33:32 -0700

    Added veloicy to the Ball, That's it

--- batter/game/ball.py
+++ batter/game/ball.py
@@ -1,3 +1,4 @@
+from game.point import Point
 from game.actor import Actor
 from game import constants
 
@@ -8,3 +9,5 @@ class Ball(Actor):
         self.set_width(constants.BALL_WIDTH)
         self.set_height(constants.BALL_HEIGHT)
         self.set_image(constants.IMAGE_BALL)
+        self.set_position(Point((constants.MAX_X /2) ,(constants.MAX_Y /3)*2))
+        self.set_velocity(Point(1,1))
